32b8003cd47cb3471d9c1495a274d821c5d940e8
Xiaolong Hong <xlhongultimate@gmail.com>
2012-03-23 13:38:30 +0800

    change the year of copyright from 2010 to 2012 in license's announcement

--- src/org/rubychina/android/api/response/HotTopicsResponse.java
+++ src/org/rubychina/android/api/response/HotTopicsResponse.java
@@ -1,4 +1,4 @@
-/*Copyright (C) 2010 Longerian (http://www.longerian.me)
+/*Copyright (C) 2012 Longerian (http://www.longerian.me)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
